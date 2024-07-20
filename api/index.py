f553dea297bdf2fce850602084d922226b17caaa
arnavchauhan <arnav216@gmail.com>
2024-07-20 15:05:00 -0400

    add some extra methods, start writing input logic

--- api/index.py
+++ api/index.py
@@ -4,14 +4,35 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+#127.0.0.1/5328
+
+@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
+def return_home():
+    return jsonify({
+        'message': 'Welcome to the home page',
+    })
+
 @app.route("/api/hello")
 def hello_world():
     return jsonify({
         'message': 'Hello, World!',
     })
 
-@app.route('/api/home', methods=['GET'])
-def return_home():
+
+@app.route('/api/test', methods=['GET'])
+def return_test():
     return jsonify({
-        'message': 'Welcome to the home page',
+        'message': 'Welcome to the test page',
     })
+
+@app.route('/api/capitalize/<string>', methods=['GET'])
+def capitalize_string(string):
+    return jsonify({
+        'message': string.capitalize(),
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5328)
+
+ 
